951fe2a38737963a5bab55112f2ee9d0ba87e231
Paul Wlodkowski <pawlodkowski@gmail.com>
2020-03-17 13:42:43 +0100

    initial commit

---
+++ wine_model.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_wine
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+DATA = load_wine()
+df = pd.DataFrame(DATA['data'], columns=DATA['feature_names'])
+y = DATA['target']  # cultivator
+
+def train_model(X, y):
+
+    # print(DATA['DESCR'])
+
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(X, y)
+    return model
+
+if __name__ == '__main__':
+    train_model(df, y)
